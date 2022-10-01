b7d5d8ee3384ed76ed66766bd0469080c8b15a85
spencer <vispencer27@gmail.com>
2022-10-01 13:40:55 -0400

    Basic quiz v1.0 ;

---
+++ basic_quiz.py
@@ -0,0 +1,75 @@
+def new_game():
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+
+    for key in questions:
+        print("--------------------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter A, B, C, or D: ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
+
+
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT!")
+        return 1
+    else:
+        print("WRONG!")
+        return 0
+
+
+def display_score(correct_guesses, guesses):
+    print("-----------------------------")
+    print("RESULTS")
+    print("-----------------------------")
+
+    print("Answers: ", end=" ")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+
+    print("Guesses: ", end=" ")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int ((correct_guesses/len(questions)) * 100)
+    print("Your score is: "+str(score)+"%")
+
+
+def play_again():
+
+    response = input("Do you want to play again ? (yes or no): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
+
+questions = {
+    "Kiyes ki kreye Python?: ": "D",
+    "Ki dat endepandans Dayiti?: ": "D",
+    "Ak kiyès Blondedy marye?: ": "D"
+}
+
+options = [["A. Spencer Vital", "B. Andersen Amazan", "C. Judlet Stanis", "D. Guido van Rossum"],
+           ["A. 1904", "B. 1803", "C. 1791", "D. 1804"],
+           ["A. Jimmy Danjer", "B. Moise Jean Charles", "C. Baky", "D. D-Perfect"]]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("Byeee !")
